c0862284f4db11dfe246caf843968429089ad7c1
dost <dost@DESKTOP-5E93V6M>
2023-01-12 19:29:07 -0500

    Again but this time commit and push together

--- src/practice/GitPracticeClass.java
+++ src/practice/GitPracticeClass.java
@@ -3,6 +3,10 @@ package practice;
 public class GitPracticeClass {
 	public static void main(String[] args) {
 		System.out.println("Let's learn GitHub");
+		
+	}
+	public static void addNumbers(int a , int b) {
+		
 	}
 
 }
